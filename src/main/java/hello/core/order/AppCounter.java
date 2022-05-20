d69422e90712c90d90fefac994ade16fc2d49e07
1000marin <leexjin@gmail.com>
2022-05-21 08:29:35 +0900

    의존관계 자동 주입 - 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/AppCounter.java
+++ src/main/java/hello/core/order/AppCounter.java
@@ -3,21 +3,5 @@ package hello.core.order;
 public class AppCounter {
     public static void main(String[] args) {
         int won;
-        int sss;
-        long yes;
-        long ll;
-        long kkkk;
-        int nu;
-        long chicken;
-        int num;
-        long ccc;
-        int yen;
-        long dd;
-        long kim;
-        long lee;
-        long ddd;
-        long pizza;
-        long python;
-        long java;
     }
 }
