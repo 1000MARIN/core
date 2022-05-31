f5f3864f2b4dbf61a5117cde7ece95462802c4a3
1000marin <leexjin@gmail.com>
2022-06-01 08:55:06 +0900

    의존관계 자동 주입 - 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/AppCounter.java
+++ src/main/java/hello/core/order/AppCounter.java
@@ -1,9 +1,10 @@
 package hello.core.order;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class AppCounter {
     public static void main(String[] args) {
-
-
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
